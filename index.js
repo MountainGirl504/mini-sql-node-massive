2115b619b8447092dd81a9f9e970967f0b11dec2
MountainGirl504 <natalia_calt@yahoo.com>
2017-10-12 19:26:53 -0600

    Complete

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
+
 app.use( bodyParser.json() );
 app.use( cors() );
+
+massive = require ('massive');
+require ('dotenv').config();
+
+massive(process.env.CONNECTION_STRING).then( dbInstance => app.set('db', dbInstance));
+// dbInstance.new_planes()
+//     .then (planes => console.log(plaines))
+//     .catch (err => console.log( err ));
+
+dbInstance.get_planes()
+    .then(planes => console.log(planes))
+    .catch(err=> console.log( err ));
 
 const port = process.env.PORT || 3000
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
